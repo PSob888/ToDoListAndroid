12667751b79c274a7ecbfcd192dcd9b08756b80b
PSob888 <235975@edu.p.lodz.pl>
2023-07-01 14:56:30 +0200

    Added itemRepository

---
+++ app/src/main/java/com/example/todolist/ItemRepository.java
@@ -0,0 +1,27 @@
+package com.example.todolist;
+
+import android.app.Application;
+
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class ItemRepository {
+    ItemDatabase itemDatabase;
+    ItemDAO ItemDao;
+    private LiveData<List<Item>> listItems;
+
+    public ItemRepository(Application application) {
+        itemDatabase = ItemDatabase.getDatabase(application);
+        ItemDao = itemDatabase.itemDAO();
+        listItems = ItemDao.getItems();
+    }
+
+    public void insertItem(Item Item) {
+        ItemDatabase.databaseWriteExecutor.execute(() -> ItemDao.insert(Item));
+    }
+
+    public LiveData<List<Item>> getAllItems() {
+        return listItems;
+    }
+}
